0a70b90e2bcbbd1a7bc4dd0920696cca9edccf49
Julio <julio.olvr@gmail.com>
2019-03-29 10:28:55 -0300

    Step 1

--- 1-hello-person/src/main.rs
+++ 1-hello-person/src/main.rs
@@ -4,6 +4,8 @@
 
 #[cfg(test)] mod tests;
 
+use rocket::http::RawStr;
+
 // FIXME: Declare the following routes:
 //
 //   * `simple_hello`
@@ -22,6 +24,34 @@
 //
 //      where 0 < age <= usize::max_value()
 //
+
+struct ValidAge(usize);
+
+impl<'r> rocket::request::FromParam<'r> for ValidAge {
+    type Error = &'r RawStr;
+
+    fn from_param(param: &'r RawStr) -> Result<Self, Self::Error> {
+        match usize::from_param(param) {
+            Ok(age) if age <= 122 => Ok(ValidAge(age)),
+            _ => Err(RawStr::from_str("Invalid age"))
+        }
+    }
+}
+
+#[get("/<name>")]
+fn simple_hello(name: String) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[get("/<name>/<age>")]
+fn good_aged_hello(name: String, age: ValidAge) -> String {
+    format!("Hello, {} year old {}.", age.0, name)
+}
+
+#[get("/<name>/<age>", rank = 2)]
+fn bad_aged_hello(name: String, age: usize) -> String {
+    format!("'{}' is a funky age, {}.", age, name)
+}
 
 fn main() {
     rocket::ignite()
